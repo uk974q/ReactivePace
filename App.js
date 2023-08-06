58cfa9b6c5ebc40c0a3ddcd30f7b2a16e5fdb07f
ullasm <ullasm@gmail.com>
2023-08-06 19:23:53 +0530

    Manual approach

---
+++ App.js
@@ -0,0 +1,4 @@
+let heading = React.createElement("h2",{id:"title"},"This is the child")
+let root = ReactDOM.createRoot(document.getElementById("root"))
+console.log("Root",root)
+root.render(heading)
