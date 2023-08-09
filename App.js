118159843dd1c67e67e65ea8a294bb81e2468ee3
ullasm <ullasm@gmail.com>
2023-08-09 10:19:43 +0530

    Added parcel and react npm packages

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM  from "react-dom/client"
+
 let heading = React.createElement("h2",{id:"title"},"This is the child")
 let root = ReactDOM.createRoot(document.getElementById("root"))
 console.log("Root",root)
