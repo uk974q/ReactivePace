bfdc89345d3e43b8f1935adca11bfc0eae3d10ea
ullasm <ullasm@gmail.com>
2023-08-11 23:33:10 +0530

    Layout planned

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React from "react"
 import ReactDOM  from "react-dom/client"
 
-// let heading = React.createElement("h2",{id:"title"},"This is the child")
 
-const reactHeadingElement = <h2 id="title" className="title-name">This is a React Element</h2> 
 
 const ReactFunctionalComponent = () => (
     <div className="container">
-        {reactHeadingElement}
+        //Header
+            //Logo
+            //Links
+        //Body
+            //Search
+            //Card layout
+                //Card
+        //Footer
     </div>
 )
 let root = ReactDOM.createRoot(document.getElementById("root"))
-// console.log("Root",root)
 root.render(<ReactFunctionalComponent/>)
