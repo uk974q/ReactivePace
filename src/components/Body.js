ee9d14392b886235e600ffd2775937f5d35e0284
ullasm <ullasm@gmail.com>
2023-08-14 22:23:31 +0530

    useEffect and Shimmer UI added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,10 @@
 import Card from "./Card"
-import { responseList } from "../utils/mockData"
-import { useState } from "react"
-
-
-
+import { useEffect, useState } from "react"
+import ShimmerUICard from "./ShimmerUICard"
 
 const Body = () => {
-    const [restaurants, setRestaurants] = useState(responseList)
-    let cardList = restaurants.map(el => (<Card resData={el} key={el.info.id} />))
+    const [restaurants, setRestaurants] = useState([])
+    let cardList = restaurants?.map(el => (<Card resData={el} key={el.info.id} />))
 
     function filterTopList(){
         let filteredList = responseList.filter(el => el.info.avgRating > 4.5)
@@ -15,15 +12,29 @@ const Body = () => {
         cardList = restaurants.map(el => (<Card resData={el} key={el.info.id} />))
     }
 
+    useEffect(() => {
+        fetchData()
+    },[])
+
+    const fetchData = async () => {
+        let res = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=9.9312328&lng=76.26730409999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        res = await res.json()
+        let responseList = (res?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        setRestaurants(responseList)
+    }
 
     return (
         <div className="body-container">
             <div className="filter">
                 <button className="btn btn-filter" onClick={() => filterTopList()}>Filter Top Rated</button>
             </div>
-            <div className="card-layout">
-                {cardList}
-            </div>
+            {!restaurants.length && <ShimmerUICard />}
+            {
+                restaurants.length > 1 && 
+                <div className="card-layout">
+                    {cardList}
+                </div>
+            }
         </div>
     )
 }
---
+++ src/components/ShimmerUICard.js
@@ -0,0 +1,14 @@
+const ShimmerUICard = () => {
+    return(
+        <div className="shimmer-card">
+            <div className="shimmer-img"></div>
+            <div className="shimmer-card-info">
+                <h1 className="shimmer-card-title"></h1>
+                <p className="shimmer-cuisines"></p>
+                <p className="shimmer-area"></p>
+            </div>
+        </div>
+    )
+}
+
+export default ShimmerUICard
