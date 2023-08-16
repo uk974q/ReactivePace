e197794a33b1fc89b16b39cffeebaf1ee29a0b0e
ullasm <ullasm@gmail.com>
2023-08-17 00:23:25 +0530

    Child Routes introduced

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM  from "react-dom/client"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
 import "./App.css"
 import Header from "./components/Header"
 import Body from "./components/Body"
@@ -11,7 +11,7 @@ import ErrorPage from "./components/ErrorPage"
 const ReactFunctionalComponent = () => (
     <div className="container">
         <Header />
-        <Body />
+        <Outlet />
     </div>
 )
 
@@ -19,16 +19,23 @@ const appRouter = createBrowserRouter([
     {
         path:"/",
         element: <ReactFunctionalComponent />,
+        children:[
+            {
+                path: "/",
+                element: <Body />
+            },
+            {
+                path:"/about",
+                element: <About />
+            },
+            {
+                path: "/contact",
+                element: <Contact />
+            }
+        ],
         errorElement: <ErrorPage />
     },
-    {
-        path:"/about",
-        element: <About />
-    },
-    {
-        path: "/contact",
-        element: <Contact />
-    }
+    
 ])
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom"
+
 const Header = () => {
     return (
         <div className="header-container">
             <div className="logo"><i className="las la-utensils"></i><span>Reactive Delivery</span></div>
             <div className="links">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact</li>
+                    <li><Link to={"/"}>Home</Link></li>
+                    <li><Link to={"/about"}>About Us</Link></li>
+                    <li><Link to={"/contact"}>Contact</Link></li>
                     <li>Cart</li>
                 </ul>
             </div>
