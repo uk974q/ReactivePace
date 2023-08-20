9d13e7f3349cdb6881a8281b6dd73bf7df2cc556
ullasm <ullasm@gmail.com>
2023-08-20 22:49:35 +0530

    state and props in class component

--- src/components/User.js
+++ src/components/User.js
@@ -5,6 +5,9 @@ const User = ({name}) => {
     return(
         <div className="user-card">
             <h2>{name} - {admin ? "Admin" : "User"}</h2>
+            <button className="btn type-change-button" onClick={() => setAdmin(val => !val)}>
+                Change Privilage
+            </button>
             <h3>India</h3>
             <h3>xyz@react.com</h3>
         </div>
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,12 +2,19 @@ import React from "react"
 class UserInfo extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            admin : true
+        } 
     }
     render(){
         const {name} = this.props
+        const {admin} = this.state
         return(
             <div className="user-card">
-                <h2>{name}</h2>
+                <h2>{name} - {admin ? "Admin" : "User"}</h2>
+                <button className="btn type-change-button" onClick={() => this.setState({admin: !admin})}>
+                    Change Privilage
+                </button>
                 <h3>India</h3>
                 <h3>xyz@react.com</h3>
             </div>
