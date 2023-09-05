52b58006367a293e215d5991a88cfea818f2384c
ullasm <ullasm@gmail.com>
2023-09-05 16:22:04 +0530

    Added icon for cart

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -2,9 +2,10 @@ import { IMAGE_CDN } from "../utils/constants"
 
 const MenuItem = ({name,src}) => {
     return(
-        <div className="flex h-14 my-2 items-center border-black border-2 rounded-lg p-2">
-            <img src={IMAGE_CDN + src} className="h-full mx-2"></img>
-            <li>{name}</li>
+        <div className="grid my-2 border-black border-2 rounded-lg p-2 grid-cols-4 col-span-1 gap-1 items-center">
+            <img src={IMAGE_CDN + src} className="w-1/2 h-full mx-2"></img>
+            <li className="col-span-2">{name}</li>
+            <i className="col-span-1 las la-cart-plus text-center text-blue-600 cursor-pointer hover:text-red-400"></i>
         </div>
     )
 }
